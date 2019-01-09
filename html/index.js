a0673d49c67df8d93cdaf29ee9a5e660a1170379
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-01-09 17:27:50 +1030

    Stream response body to decoder if supported

--- html/index.js
+++ html/index.js
@@ -1,8 +1,6 @@
 import Module from '/vcddec.js';
 import Decoder from '/decoder.js';
 
-const textDec = new TextDecoder('utf-8');
-
 const must200 = resp => {
 	if (!resp.ok) {
 		throw new Error('failed to load ' + resp.url + ': ' + resp.statusText);
@@ -10,10 +8,29 @@ const must200 = resp => {
 
 	return resp;
 };
+
 const asUint8Array = async resp => new Uint8Array(await resp.arrayBuffer());
 
+const decodeResp = async (dec, resp) => {
+	if (!resp.body) {
+		return dec.decode(await asUint8Array(resp));
+	}
+
+	const reader = resp.body.getReader();
+	for (; ;) {
+		const { done, value } = await reader.read();
+		if (done) {
+			return;
+		}
+
+		dec.decode(value);
+	}
+};
+
 const dictFetch = fetch('/test.dict').then(must200).then(asUint8Array);
-const dataFetch = fetch('/test.diff').then(must200).then(asUint8Array);
+const dataFetch = fetch('/test.diff').then(must200);
+
+const textDec = new TextDecoder('utf-8');
 
 async function moduleLoaded(m) {
 	const dec = new Decoder(m, s => {
@@ -21,7 +38,7 @@ async function moduleLoaded(m) {
 	});
 	try {
 		dec.start(await dictFetch);
-		dec.decode(await dataFetch);
+		await decodeResp(dec, await dataFetch);
 		dec.finish();
 	} finally {
 		dec.destroy();
