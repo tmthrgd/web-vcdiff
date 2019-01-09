d398a02f39e58b67e57b9648a9fd689f3f34fcc2
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-01-09 11:13:00 +1030

    Format C++ code w/ clang-format

--- output_string.h
+++ output_string.h
@@ -1,8 +1,8 @@
 #include <google/output_string.h>
 
 class OutputJSBase : public open_vcdiff::OutputStringInterface {
-public:
-	virtual OutputJSBase& append(const char* s, size_t n) {
+      public:
+	virtual OutputJSBase &append(const char *s, size_t n) {
 		appendCallback(s, n);
 		return *this;
 	}
@@ -11,10 +11,10 @@ public:
 
 	virtual void push_back(char) { abort(); }
 
-	virtual void ReserveAdditionalBytes(size_t) { /* NOOP */ }
+	virtual void ReserveAdditionalBytes(size_t) {} /* NOOP */
 
 	virtual size_t size() const { abort(); }
 
-protected:
-	virtual void appendCallback(const char* s, unsigned int n) = 0;
+      protected:
+	virtual void appendCallback(const char *s, unsigned int n) = 0;
 };
