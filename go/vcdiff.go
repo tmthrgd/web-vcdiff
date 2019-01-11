5b6bc99dd0baf56c41cc43475566f7c259f925aa
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-01-11 19:03:11 +1030

    Move VCDIFF http.Handler into go package

---
+++ go/vcdiff.go
@@ -0,0 +1,30 @@
+package vcdiff
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func Handler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hdr := w.Header()
+		hdr.Set("Vary", "Accept-Diff-Encoding")
+
+		if !strings.EqualFold(r.Header.Get("Accept-Diff-Encoding"), "vcdiff") {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		hdr.Set("Content-Diff-Encoding", "vcdiff")
+		hdr.Set("Content-Diff-Dictionary", "/test.dict#b47cc0f104b6")
+
+		r2 := new(http.Request)
+		*r2 = *r
+		r2.URL = new(url.URL)
+		*r2.URL = *r.URL
+		r2.URL.Path += ".diff"
+
+		h.ServeHTTP(w, r2)
+	})
+}
