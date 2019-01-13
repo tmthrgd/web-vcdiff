594bbb30c3521143f92f6c2a3022ef3bb69f02de
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-01-13 11:21:40 +1030

    Merge output_string.h into vcddec.cc

--- vcddec.cc
+++ vcddec.cc
@@ -1,4 +1,26 @@
+#include <cstdlib>
 #include <google/vcdecoder.h>
 
-#include "output_string.h"
+class OutputJSBase : public open_vcdiff::OutputStringInterface {
+      public:
+	virtual OutputJSBase &append(const char *s, size_t n) {
+		appendCallback(s, n);
+		return *this;
+	}
+
+	virtual void clear() { std::abort(); }
+
+	virtual void push_back(char) { std::abort(); }
+
+	virtual void ReserveAdditionalBytes(size_t res_arg) {
+		reserveCallback(res_arg);
+	}
+
+	virtual size_t size() const { std::abort(); }
+
+      protected:
+	virtual void appendCallback(const char *s, unsigned int n) = 0;
+	virtual void reserveCallback(unsigned int n) = 0;
+};
+
 #include <vcddec_glue.cpp>
